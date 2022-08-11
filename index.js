041300055620024e73b3fa4d1b9ae74d37cc9238
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-08-11 14:04:30 -0500

    wired user input

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ inquirer
 
 function createREADME(input) {
     const textArray = processInput(input)
-    let stream = fs.createWriteStream("./append.md", {flags:'a'});
-    textArray.forEach( (index) =>{stream.write(index + "\n");});
+    let stream = fs.createWriteStream("./test/append.md", {flags:'a'});
+    textArray.forEach( (index) =>{stream.write(index + "\n\n");});
     stream.end();
 }
 
@@ -70,43 +70,52 @@ function processInput(input) {
 }
 
 function getTitle(input) {
-    let dfault = "# Title"
+    let dfault = "# Title\n"
     if (input.title == '') return dfault
-    return input.title
+    let clean = "# " + input.title
+    return clean
 }
 
 function getDescription(input) {
-    let dfault = "## Description"
+    let dfault = "## Description\n"
     if (input.desc == '') return dfault
-    return input.desc
+    let clean = dfault + input.desc 
+    return clean
 }
 
 function getInstall(input) {
-    let dfault = "## Install"
+    let dfault = "## Install\n"
     if (input.install == '') return dfault
-    return input.install
+    let clean = dfault + input.install
+    return clean
 }
 
 function getUse(input) {
-    let dfault = "## Useage"
+    let dfault = "## Useage\n"
     if (input.use == '') return dfault
-    return input.use
+    let clean = dfault + input.use
+    return clean
 }
 
 function getContributing(input) {
-    let dfault = "## Contribution"
+    let dfault = "## Contribution\n"
     if (input.cont == '') return dfault
-    return input.cont
+    let clean = dfault + input.cont
+    return clean
 }
 
 function getTests(input) {
-    let dfault = "## Tests"
+    let dfault = "## Tests\n"
     if (input.test == '') return dfault
-    return input.test
+    let clean = dfault + input.test
+    return clean
 }
 
 function getQuestion(input) {
-    let dfault = "## Questions"
+    let dfault = "## Questions\n"
     if (input.git == '') return dfault
-    return input.git
+    let clean = dfault + input.git
+    return clean
 }
+
+// license and table of contents are special cases
